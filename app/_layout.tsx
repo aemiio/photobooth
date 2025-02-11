401e400addf305926984c3efcccf9ad86567f12d
aemiio <cjyramae@gmail.com>
2025-02-12 02:49:43 +0800

    :feat: implement splash screen functionality by preventing auto-hide and adding timeout for manual hide

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,18 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
+import { SplashScreen } from "expo-router";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 100);
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
       <StatusBar style="light" />
@@ -12,24 +22,9 @@ export default function Layout() {
           animation: "none",
         }}
       >
-        <Stack.Screen
-          name="index"
-          options={{
-            animation: "none",
-          }}
-        />
-        <Stack.Screen
-          name="countdown"
-          options={{
-            animation: "none",
-          }}
-        />
-        <Stack.Screen
-          name="printing"
-          options={{
-            animation: "none",
-          }}
-        />
+        <Stack.Screen name="index" options={{ animation: "none" }} />
+        <Stack.Screen name="countdown" options={{ animation: "none" }} />
+        <Stack.Screen name="printing" options={{ animation: "none" }} />
       </Stack>
     </View>
   );
